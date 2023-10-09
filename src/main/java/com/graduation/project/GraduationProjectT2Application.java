f085f1b51ef1ee2c49a10795c8a344ec331a869d
Gukk18 <emailnhonho@gmail.com>
2023-10-09 22:20:00 +0700

    upload code

---
+++ src/main/java/com/graduation/project/GraduationProjectT2Application.java
@@ -0,0 +1,13 @@
+package com.graduation.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationProjectT2Application.class, args);
+	}
+
+}
