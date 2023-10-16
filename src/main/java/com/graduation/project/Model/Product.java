2c0c3f0ba720cabd0251ab931aad34262ac454f5
Gukk18 <emailnhonho@gmail.com>
2023-10-16 13:55:48 +0700

    Upload model

---
+++ src/main/java/com/graduation/project/Model/Product.java
@@ -0,0 +1,33 @@
+package com.graduation.project.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+    //Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    @JdbcTypeCode(SqlTypes.INTEGER)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    @JdbcTypeCode(SqlTypes.NVARCHAR)
+    private String name;
+
+
+//Name
+
+
+
+}
