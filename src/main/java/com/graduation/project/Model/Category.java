52d8460f36adef44cbb6e5cbf992a7d04fcba11c
Gukk18 <emailnhonho@gmail.com>
2023-10-16 13:55:48 +0700

    Upload model

---
+++ src/main/java/com/graduation/project/Model/Category.java
@@ -0,0 +1,39 @@
+package com.graduation.project.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name_category", nullable = false, unique = true)
+    @JdbcTypeCode(SqlTypes.NVARCHAR)
+    private String name_category;
+
+    @Column(name = "description", nullable = false, unique = true)
+    @JdbcTypeCode(SqlTypes.NVARCHAR)
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "category_id")
+    private Set<Product> products = new LinkedHashSet<>();
+
+//relationship
+
+
+
+
+}
